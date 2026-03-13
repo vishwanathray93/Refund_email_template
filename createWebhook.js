cfe6e403affdb8eb41b59bdcc96adf0a9dbc2b7f
Vishwanath <rayvishu93@gmail.com>
2026-03-13 10:15:24 +0530

    Remove sensitive credentials from createWebhook.js
    Removed sensitive credentials from createWebhook.js.

--- createWebhook.js
+++ createWebhook.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 
-const storeHash = "jlmaubflvk";
-const clientId = "dtb2sgkh1zpcxzdgu0ly7a16so2mp3u";
-const accessToken = "gft8y3fgyxnat4i4zj852f7lpdtnyvj";
+const storeHash = "";
+const clientId = "";
+const accessToken = "";
 
 // ngrok / public URL
 const destinationUrl = "https://0ae0-2405-201-4018-90a7-c49b-1c4b-a7d9-984d.ngrok-free.app/order-events";
